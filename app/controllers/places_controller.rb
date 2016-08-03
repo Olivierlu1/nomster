cad6411b954d9118d96b93c98b8167113d87b04c
olivierlu1 <olivierlu1@github.com>
2016-08-03 08:54:47 +0000

    Will paginate error and routes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,10 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
-    @posts= Place.paginate(:page => params[:page], :per_page => 5)
+    @posts = Place.paginate(:page => params[:page], :per_page => 4)
+  end
+
+  def new
+
   end
 end
