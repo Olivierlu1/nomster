b7c7858adcb16a984dc209a19ca1034d108a66b3
olivierlu1 <olivierlu1@github.com>
2016-08-06 09:18:47 +0000

    Adding userid

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'sass-rails'
 gem 'will_paginate', '~> 3.1.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails_12factor', group: :production
-gem 'devise', '~> 3.2.4'
+gem 'devise', '>= 3.2.4'
 
 gem 'simple_form'
 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @places = Place.all
     @posts = Place.paginate(:page => params[:page], :per_page => 4)
@@ -10,9 +12,11 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
+
+  private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
