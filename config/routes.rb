68402a0013e194cc6376a3353eb18f69da4e61cf
olivierlu1 <olivierlu1@github.com>
2016-08-14 09:04:28 +0000

    Added user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     resources :photos, only: :create
   end
 
+  resources :users, only: :show
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
